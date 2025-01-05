3c0968c118a6b959aa778b5c81b3e964bd8b962e
JakubSasik <j.sasik6@gmail.com>
2025-01-05 18:39:02 +0100

    Update dashboard_visualizations.sql

--- dashboard_visualizations.sql
+++ dashboard_visualizations.sql
@@ -2,7 +2,7 @@
 SELECT 
     c.Country AS country,
     SUM(fs.total_amount) AS total_sales
-FROM fact_sales fs
+FROM  fact_sales fs
 JOIN dim_customers c ON fs.dim_customerId = c.dim_customerId
 GROUP BY c.Country
 ORDER BY total_sales DESC
@@ -14,18 +14,18 @@ SELECT
     g.genre_name AS genre,
     SUM(fs.total_amount) AS total_sales
 FROM fact_sales fs
-JOIN dim_tracks t ON fs.dim_trackId = t.dim_trackId
-JOIN dim_genres_media g ON t.GenreId = g.dim_genreId
+JOIN dim_genre g ON fs.dim_genre_id = g.dim_genre_id
 GROUP BY g.genre_name
-ORDER BY total_sales DESC;
+ORDER BY total_sales DESC
+LIMIT 5;
 
 -- Graf 3: Trend predajov v čase
 SELECT 
     d.year,
     COUNT(DISTINCT fs.dim_customerId) AS unique_customers,
-    COUNT(fs.fact_salesId) AS total_transactions
+    COUNT(fs.fact_sales_id) AS total_transactions
 FROM fact_sales fs
-JOIN dim_date d ON fs.dateID = d.dim_dateId
+JOIN dim_date d ON fs.dim_dateId = d.dim_dateId
 WHERE d.year BETWEEN YEAR(CURRENT_DATE) - 5 AND YEAR(CURRENT_DATE)
 GROUP BY d.year
 ORDER BY d.year;
@@ -37,20 +37,23 @@ SELECT
     SUM(fs.total_amount) AS total_sales
 FROM fact_sales fs
 JOIN dim_customers c ON fs.dim_customerId = c.dim_customerId
-JOIN dim_date d ON fs.dateID = d.dim_dateId
+JOIN dim_date d ON fs.dim_dateId = d.dim_dateId  
 WHERE d.year = 2024
 GROUP BY c.full_name
 ORDER BY total_sales DESC
 LIMIT 5;
 
+
 -- Graf 5: Predaje podľa mediálnych formátov v kvartáloch počas covidu
 SELECT 
-    d.quarter, 
-    m.media_type, 
+    d.year,
+    d.quarter,
+    m.media_type_name,
     SUM(fs.total_amount) AS total_sales
 FROM fact_sales fs
-JOIN dim_genres_media m ON fs.media_type = m.dim_mediaTypeId
-JOIN dim_date d ON fs.dateID = d.dim_dateId
+JOIN dim_date d ON fs.dim_dateId = d.dim_dateId
+JOIN dim_media_type m ON fs.dim_media_type_id = m.dim_media_type_id
 WHERE d.year BETWEEN 2020 AND 2022
-GROUP BY d.quarter, m.media_type
-ORDER BY d.quarter, m.media_type;
+GROUP BY d.year, d.quarter, m.media_type_name
+ORDER BY d.year, d.quarter, total_sales DESC;
+
